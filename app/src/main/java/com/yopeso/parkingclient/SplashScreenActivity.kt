19f8fb83dc9f6fc40cf65020e199cc90259e55f2
James Oanta <james.oanta@yopeso.com>
2017-07-05 17:14:43 +0300

    Initial commit: added splash screen.

---
+++ app/src/main/java/com/yopeso/parkingclient/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.yopeso.parkingclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
+
