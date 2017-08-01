92cddc3e0c98fc8500f7134079ec5cb585dcffc1
Eugen Ungurean <eugen.ungurean@yopeso.com>
2017-08-01 21:49:22 +0300

    add crashlytics

--- app/src/main/java/com/yopeso/parkingclient/App.kt
+++ app/src/main/java/com/yopeso/parkingclient/App.kt
@@ -1,12 +1,16 @@
 package com.yopeso.parkingclient
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.facebook.drawee.backends.pipeline.Fresco
+import io.fabric.sdk.android.Fabric
+
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Fresco.initialize(this)
+        Fabric.with(this, Crashlytics())
     }
 }
